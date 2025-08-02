d5a146a584420632a625698df914ea864783b4c7
BobJohnson <143335904+BobJohnson105@users.noreply.github.com>
2025-08-02 01:40:04 +0100

    Reapply "Merge branch 'main' of https://github.com/BobJohnson105/Doomscroller"
    This reverts commit 0aa93621213b53599110387f20b152ab748aa561.

--- Assets/Scripts/Reel.cs
+++ Assets/Scripts/Reel.cs
@@ -1,32 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Reel : MonoBehaviour
 {
+    public float FPS = 2.0f;
     public float Pos
     {
         get => m_fPos;
         set
         {
             m_fPos = value;
+            if ( !m_pRectTransform )
+                m_pRectTransform = GetComponent<RectTransform>();
             m_pRectTransform.anchorMin = new( m_pRectTransform.anchorMin.x, m_fPos );
             m_pRectTransform.anchorMax = new( m_pRectTransform.anchorMax.x, m_fPos + 1 );
         }
     }
-    public bool TopReel => m_fPos >= 0.0f;
     [SerializeField]
     float m_fPos;
+    RawImage m_pImage;
+
+    readonly List<Texture> m_pFrames = new();
+    int m_iAnimFrame;
+
     RectTransform m_pRectTransform;
     void OnEnable()
     {
+        m_iAnimFrame = 0;
         m_pRectTransform = GetComponent<RectTransform>();
         m_fPos = m_pRectTransform.anchorMin.y;
+
+        m_pImage = GetComponent<RawImage>();
+
+        foreach ( RawImage i in transform.parent.GetComponentInChildren<cooking1>( true ).GetComponentsInChildren<RawImage>() )
+            m_pFrames.Add( i.texture );
+
+        StartCoroutine( PlayVideo() );
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator PlayVideo()
+    {
+        while ( true )
+        {
+            yield return new WaitForSeconds( 1.0f / FPS );
+            m_iAnimFrame++;
+            m_iAnimFrame %= m_pFrames.Count;
+            m_pImage.texture = m_pFrames[ m_iAnimFrame ];
+        }
     }
 }
